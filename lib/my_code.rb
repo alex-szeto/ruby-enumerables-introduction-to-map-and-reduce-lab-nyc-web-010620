33f5aea4f0a9819474c924f4526ae3c0216c9e3b
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 02:55:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -12,11 +12,19 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-
+  output_array = []
+  source_array.each{|element|
+    output_array << element * 2
+  }
+  return output_array
 end
 
 def map_to_square(source_array)
-
+  output_array = []
+  source_array.each{|element|
+    output_array << element * element
+  }
+  return output_array
 end
 
 def reduce_to_total(source_array, starting_point)

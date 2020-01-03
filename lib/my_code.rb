4d5314a0218c2bc715c39b1b669823f2f29c420a
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 03:03:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,17 +27,12 @@ def map_to_square(source_array)
   return output_array
 end
 
-def reduce_to_total(source_array, starting_point)
-  total = 0
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
   source_array.each{|number|
     total += number
   }
-  if starting_point == nil
-    put
-    return total
-  else
-    return total + start_point
-  end
+  return total
 end
 
 def reduce_to_all_true(source_array)

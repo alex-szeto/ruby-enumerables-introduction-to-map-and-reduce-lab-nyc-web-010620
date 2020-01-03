0b540a3c220fff1e5ec4d9fe78c5692e5aca8edb
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 02:53:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,2 +1,8 @@
 # My Code here....
-def map_to_negat
+def map_to_negativize(source_array)
+  output_array = []
+  source_array.each{|element|
+    output_array << element * -1
+  }
+  return output_array
+end

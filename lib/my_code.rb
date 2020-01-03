0c531e73f0a0c66b6aa3bc3aab11a26daf5b2e2e
Alexander Szeto <alex.szeto.a@gmail.com>
2020-01-03 03:05:46 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,5 +45,10 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-
+  source_array.each{|element|
+    if element == true
+      return true
+    end
+  }
+  return false
 end
